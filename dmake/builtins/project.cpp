d0bd4a43dc97ddd4d185f1f18e9c97fc3c2171b9
Martin Chang <marty1885@users.noreply.github.com>
2026-01-27 01:16:16 +0800

    make mark_as_advanced

--- dmake/builtins/project.cpp
+++ dmake/builtins/project.cpp
@@ -7,6 +7,7 @@ void register_project_builtins(Interpreter& interp) {
     interp.add_builtin("cmake_minimum_required", [](Interpreter&, const std::vector<std::string>&) {});
     interp.add_builtin("project", [](Interpreter&, const std::vector<std::string>&) {});
     interp.add_builtin("cmake_policy", [](Interpreter&, const std::vector<std::string>&) {});
+    interp.add_builtin("mark_as_advanced", [](Interpreter&, const std::vector<std::string>&) {});
 }
 
 } // namespace dmake
