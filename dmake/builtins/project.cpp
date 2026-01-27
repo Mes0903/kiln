046b71c88cf19daf4a6b9a9c84294e539e0fba56
Martin Chang <marty1885@users.noreply.github.com>
2026-01-27 12:49:06 +0800

    add project_name

--- dmake/builtins/project.cpp
+++ dmake/builtins/project.cpp
@@ -5,7 +5,15 @@ namespace dmake {
 
 void register_project_builtins(Interpreter& interp) {
     interp.add_builtin("cmake_minimum_required", [](Interpreter&, const std::vector<std::string>&) {});
-    interp.add_builtin("project", [](Interpreter&, const std::vector<std::string>&) {});
+    interp.add_builtin("project", [](Interpreter& interp, const std::vector<std::string>& args) {
+        if(args.size() == 0) {
+            interp.set_fatal_error("project() requires at least one argument");
+            return;
+        }
+
+        interp.set_variable("PROJECT_NAME", args[0]);
+        // ignore the rest - we can deal with them later
+    });
     interp.add_builtin("cmake_policy", [](Interpreter&, const std::vector<std::string>&) {});
     interp.add_builtin("mark_as_advanced", [](Interpreter&, const std::vector<std::string>&) {});
 }
